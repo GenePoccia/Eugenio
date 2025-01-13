de8b1b1a73db9232029eb0f7cdf4e6977a65be72
Eugenio Poccia <pocciaeugenio@gmail.com>
2025-01-13 02:46:50 -0500

    feat: fixing responsiveness

--- src/app/DescriptionSection.jsx
+++ src/app/DescriptionSection.jsx
@@ -7,14 +7,16 @@ import styled from "styled-components";
 const PortableTextContainer = styled.div`
 	display: flex;
 	flex-direction: column;
-
 	flex: 1;
 	max-width: 45%;
 
 	@media (max-width: 1100px) {
+		margin-top: 5%;
+		align-items: center;
 		max-width: none;
 	}
 `;
+
 const DescriptionSection = ({ description }) => {
 	return (
 		<PortableTextContainer>
