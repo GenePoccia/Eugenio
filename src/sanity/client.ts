b96de40b35dc091844bbbd5366e0d51d9a1c071d
Eugenio Poccia <pocciaeugenio@gmail.com>
2025-01-15 17:52:06 -0500

    feat: variable name change

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-	projectId: process.env.PROJECT_ID,
-	dataset: process.env.DATASET,
+	projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+	dataset: process.env.NEXT_PUBLIC_DATASET,
 	apiVersion: "2024-01-01",
 	useCdn: false,
 });
