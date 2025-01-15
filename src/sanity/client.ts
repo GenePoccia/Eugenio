59f515739c532513bf9368f611b8ba75a61b65b1
Eugenio Poccia <pocciaeugenio@gmail.com>
2025-01-15 17:28:03 -0500

    chore: clean

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-	projectId: process.env.PROJECT_ID || "usa4ylim",
-	dataset: process.env.DATASET || "production",
+	projectId: process.env.PROJECT_ID,
+	dataset: process.env.DATASET,
 	apiVersion: "2024-01-01",
 	useCdn: false,
 });
